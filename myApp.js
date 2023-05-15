0b956d544308185c8270f4cf0684e02948763f2d
Clarktms <clarktms@gmail.com>
2023-05-15 16:19:27 -0500

    update

--- myApp.js
+++ myApp.js
@@ -1,39 +1,25 @@
-let express = require('express');
+let express = require("express");
 let app = express();
+require("dotenv").config();
 
+app.use("/public", express.static(__dirname + "/public"));
 
+app.use("/", (req, res, next) => {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+});
 
+console.log("Hello World");
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
+// app.get("/json", (req, res) => {
+//   if (process.env.MESSAGE_STYLE === "uppercase") {
+//     res.json({ message: "HELLO JSON" });
+//   } else {
+//     res.json({ message: "Hello json" });
+//   }
+// });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
